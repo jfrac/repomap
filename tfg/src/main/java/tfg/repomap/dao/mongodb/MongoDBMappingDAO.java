082fadb7cfb94d20ec0e6a4ee1abc60c941551e2
Juanfra <frearf@gmail.com>
2016-03-16 18:42:17 +0100

    Mongo DAO

---
+++ tfg/src/main/java/tfg/repomap/dao/mongodb/MongoDBMappingDAO.java
@@ -0,0 +1,124 @@
+package tfg.repomap.dao.mongodb;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.Collection;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException.DuplicateKey;
+import com.mongodb.util.JSON;
+
+import tfg.repomap.dao.AbstractMappingDAO;
+import tfg.repomap.dao.MappingDAOException;
+import tfg.repomap.mapping.Mapping;
+import tfg.repomap.mapping.MappingId;
+import tfg.repomap.scheme.Scheme;
+
+public class MongoDBMappingDAO extends AbstractMappingDAO
+{
+	protected DB db;
+
+	public MongoDBMappingDAO() {
+		try {
+			MongoClient mongoClient = new MongoClient("localhost", 27017);
+			db = mongoClient.getDB("repomap");
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public Mapping create(Scheme sourceScheme, Scheme targetScheme) 
+			throws MappingDAOException {
+		Mapping mapping = super.create(sourceScheme, targetScheme);
+		save(mapping);
+		return mapping;
+	}
+
+	protected void save(Mapping mapping) throws MappingDAOException {
+		try {
+			String mappingJSON = mappingToJSON(mapping);
+			DBObject dbObject = (DBObject)JSON.parse(mappingJSON);
+			dbObject.put("_id", mapping.getId().getId());
+			db.getCollection("mappings").insert(dbObject);
+		} catch (DuplicateKey e) {
+			update(mapping);
+		} catch (Exception e) {
+			throw new MappingDAOException();
+		}
+	}
+
+	@Override
+	public Mapping findById(MappingId mappingId) throws MappingDAOException {
+		DBCollection collection = db.getCollection("mappings");
+		BasicDBObject query = new BasicDBObject();
+		query.append("_id", mappingId.getId());
+		DBObject dbObj = collection.findOne(query);
+		if (dbObj == null) {
+			return null;
+		}
+		dbObj.removeField("_id");
+		
+		Gson gson = getGson();
+		Mapping mapping;
+		try {
+			mapping = gson.fromJson(dbObj.toString(), Mapping.class);
+			return mapping;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			throw new MappingDAOException();
+		}
+	}
+
+	@Override
+	public Collection<Mapping> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Mapping mapping) throws MappingDAOException {
+		try {
+			BasicDBObject query = new BasicDBObject();
+			query.append("_id", mapping.getId().getId());
+			String mappingJSON = mappingToJSON(mapping);
+			DBObject dbObjectMapping = (DBObject)JSON.parse(mappingJSON);
+			getCollection().update(query, dbObjectMapping);
+		} catch (IOException e) {
+			throw new MappingDAOException();
+		}
+	}
+
+	@Override
+	public boolean remove(MappingId mappingId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	protected DBCollection getCollection() {
+		return db.getCollection("mappings");
+	}
+		
+	protected String mappingToJSON(Mapping mapping) 
+			throws JsonGenerationException, JsonMappingException, IOException {
+		Gson gson = getGson();
+		String mappingJSON = gson.toJson(mapping, Mapping.class);
+		return mappingJSON; 
+	}
+	
+	protected Gson getGson() {
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		gsonBuilder.registerTypeAdapter(Scheme.class, new SchemeAdapter());
+		Gson gson = gsonBuilder.create();
+		return gson;
+	}
+}
---
+++ tfg/src/main/java/tfg/repomap/dao/mongodb/SchemeAdapter.java
@@ -0,0 +1,39 @@
+package tfg.repomap.dao.mongodb;
+
+import java.lang.reflect.Type;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import tfg.repomap.scheme.Scheme;
+
+class SchemeAdapter implements JsonSerializer<Scheme>, JsonDeserializer<Scheme> {
+    @Override
+    public JsonElement serialize(Scheme src, Type typeOfSrc, JsonSerializationContext context) {
+        JsonObject result = new JsonObject();
+        result.add("type", new JsonPrimitive(src.getClass().getCanonicalName()));
+        result.add("properties", context.serialize(src, src.getClass()));
+
+        return result;
+    }
+
+    @Override
+    public Scheme deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+        throws JsonParseException {
+        JsonObject jsonObject = json.getAsJsonObject();
+        String type = jsonObject.get("type").getAsString();
+        JsonElement element = jsonObject.get("properties");
+
+        try {
+            return context.deserialize(element, Class.forName(type));
+        } catch (ClassNotFoundException cnfe) {
+            throw new JsonParseException("Unknown element type: " + type, cnfe);
+        }
+    }
+}
