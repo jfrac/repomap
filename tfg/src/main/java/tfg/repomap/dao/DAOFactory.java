135236d6921317204a44c248fc3cc3092ffa0088
Juanfra <frearf@gmail.com>
2016-07-16 20:32:38 +0200

    Dao factory get class from system properties

--- tfg/src/main/java/tfg/repomap/dao/DAOFactory.java
+++ tfg/src/main/java/tfg/repomap/dao/DAOFactory.java
@@ -1,11 +1,44 @@
 package tfg.repomap.dao;
 
-import tfg.repomap.dao.file.FileMappingDAO;
+import tfg.repomap.dao.mongodb.MongoDBMappingDAO;
 
 public abstract class DAOFactory {
 	
+	private static final Class<?> DEFAULT_FACTORY = MongoDBMappingDAO.class;
+	public static final String DAO_FACTORY = "bookle.dao.factory";
+	
 	public static MappingDAO getDAO() {
-		// TODO use JVM properties
-		return new FileMappingDAO();
+		Class<?> daoClass = getDAOClass();
+		
+		AbstractMappingDAO mappingDAO = null;
+		
+		try {
+			mappingDAO = (AbstractMappingDAO) daoClass.newInstance();
+		} catch (Exception e) {			
+			throw new RuntimeException(e);
+		}
+		
+		return mappingDAO;
+	}
+	
+	protected static Class<?> getDAOClass() {
+		String daoClassName = System.getProperty(DAO_FACTORY);
+		
+		if (daoClassName == null)
+			return DEFAULT_FACTORY;
+		
+		Class<?> daoClass = null;
+		
+		try {
+			daoClass = Class.forName(daoClassName);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		
+		if (AbstractMappingDAO.class.isAssignableFrom(daoClass)) {
+			return daoClass;
+		}
+		
+		return null;
 	}
 }
