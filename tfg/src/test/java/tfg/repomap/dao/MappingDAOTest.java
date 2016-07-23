cca86cd0f3e55ddff2b91a44a5a14ea9cabb792c
Juanfra <frearf@gmail.com>
2016-07-23 20:37:38 +0200

    Fixed DAO test

--- tfg/src/test/java/tfg/repomap/dao/MappingDAOTest.java
+++ tfg/src/test/java/tfg/repomap/dao/MappingDAOTest.java
@@ -28,7 +28,14 @@ extends TestCase
 
 	@Test
 	public void crudMappingTest() 
-		throws MappingDAOException, Property2PropertyAlreadyExists, Property2PropertyNotValid, Relation2RelationAlreadyExists, SchemeException, EntityNotFoundException, Entity2EntityExistsException {
+		throws MappingDAOException, 
+		       Property2PropertyAlreadyExists, 
+		       Property2PropertyNotValid, 
+		       Relation2RelationAlreadyExists, 
+		       SchemeException, 
+		       EntityNotFoundException, 
+		       Entity2EntityExistsException {
+		
 		Mapping mapping = MappingBuilder.newMapping();
 		MappingId mappingId = mapping.getId();
 		
@@ -57,9 +64,10 @@ extends TestCase
 			mapping = dao.findById(mappingId);
 			assertTrue(mapping.contains(property2Property));
 			
+			
 			Relation2Relation relation2Relation = new Relation2Relation(
-				new Relation("complexType", "topLevelComplexType"),
-				new Relation("Researcher", "Product")
+				new Relation("complexType", "simpleType"),
+				new Relation("Researcher", "Student")
 			);
 			mapping.addRelation2Relation(relation2Relation);
 			dao.update(mapping);
@@ -71,7 +79,5 @@ extends TestCase
 			mapping = dao.findById(mappingId);
 			assertNull(mapping);
 			
-		
-		
 	}
 }
