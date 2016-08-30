15ec9d8ebd62de02173b041287d3cc3b1af06dea
Juanfra <frearf@gmail.com>
2016-08-30 22:13:44 +0200

    REST test added to suite

--- tfg/src/test/java/tfg/repomap/AppTest.java
+++ tfg/src/test/java/tfg/repomap/AppTest.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import api.rest.RESTTest;
 import tfg.repomap.dao.file.FileMappingDAOTest;
 import tfg.repomap.dao.mongodb.MongoDBMappingDAOTest;
 import tfg.repomap.mapping.MappingTest;
@@ -13,7 +14,8 @@ import tfg.repomap.mapping.MappingTest;
 @SuiteClasses({
 	MappingTest.class, 
 	MongoDBMappingDAOTest.class,
-	FileMappingDAOTest.class})
+	FileMappingDAOTest.class,
+	RESTTest.class})
 public class AppTest 
 {
     
